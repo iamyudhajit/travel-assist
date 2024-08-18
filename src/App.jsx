5775c5169582daf1ec087e963552a5953f930177
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2024-08-18 15:38:51 +0530

    Sorting Done.

--- src/App.jsx
+++ src/App.jsx
@@ -110,13 +110,38 @@ function Item({ item , onDeleteItem , onCheckedItem}) {
 
 function PackingList({items , onDeleteItem, onCheckedItem}) {
 
+  const[sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if(sortBy=='input') sortedItems = items;
+  if(sortBy=='description') sortedItems = items.slice().sort((a,b)=>a.description.localeCompare(b.description));
+  if(sortBy=='packed') sortedItems = items
+    .slice()
+    .sort((a, b) => Number(a.packed)-Number(b.packed));
+  
+
   return (
     <div className="list">
       <ul>
-        {items.map((item, index) => {
-          return <Item item={item} key={index} onDeleteItem = {onDeleteItem} onCheckedItem={onCheckedItem}/>;
+        {sortedItems.map((item, index) => {
+          return (
+            <Item
+              item={item}
+              key={index}
+              onDeleteItem={onDeleteItem}
+              onCheckedItem={onCheckedItem}
+            />
+          );
         })}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description order</option>
+          <option value="packed">Sort By packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
